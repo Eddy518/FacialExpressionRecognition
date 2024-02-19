952789d82a186883f7d739bcbe5fa512da35f5fc
Eddy518 <edmwangi2@gmail.com>
2024-02-19 22:34:57 +0300

    fix: Remove forgot password for sign up page

--- app.py
+++ app.py
@@ -2,20 +2,20 @@ from flask import Flask,render_template,url_for
 
 app = Flask(__name__)
 
-@app.route('/')
 @app.route('/home')
+@app.route('/')
 def home():
-    return render_template('index.html')    
+    return render_template('index.html',title='Home')    
 
 @app.route('/login')
-def log_in():
-    return render_template('login.html')
+def login():
+    return render_template('login.html',title='Log In')
 
 @app.route('/register')
 @app.route('/sign-up')
 @app.route('/signup')
 def register():
-    return render_template('sign-up.html')
+    return render_template('sign-up.html',title='Sign Up')
 
 if __name__ == '__main__':
     app.run(debug=True)
