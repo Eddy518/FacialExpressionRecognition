f7dbfe9d298ea4d95c9fe3a12b4ad0fdb8259cbe
Eddy518 <edmwangi2@gmail.com>
2024-02-24 21:37:24 +0300

    feat: Add user authentication with sqlite
    BREAKING CHANGE
    All previous successful validations are now stored in an SQLite database

--- facial/form.py
+++ facial/form.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from facial.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('UserName:',validators=[DataRequired(),Length(min=2,max=15)])
@@ -13,6 +14,16 @@ class RegistrationForm(FlaskForm):
     
     submit = SubmitField('Sign up')
     
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username is taken. Please try another username')
+        
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email already exists.')
+
 class LoginForm(FlaskForm):
     email = StringField("Your Email:",validators=[DataRequired(),Email()])
     
--- facial/routes.py
+++ facial/routes.py
@@ -1,6 +1,9 @@
-from flask import render_template,url_for, flash, redirect
-from facial import app
+from flask import render_template,url_for, flash, redirect, request
+from facial import app,bcrypt, db
 from facial.form import RegistrationForm,LoginForm
+from facial.models import User
+from flask_login import login_user, current_user, logout_user, login_required
+
 forgot = False
 
 @app.route('/home')
@@ -10,11 +13,15 @@ def home():
 
 @app.route('/login',methods=('GET','POST'))
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == "password":
-            flash('You have been logged in!','success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password,form.password.data):
+            login_user(user)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('dashboard'))
         else:
             flash('Login failed! Invalid credentials!','fail')
     return render_template('login.html',title='Log In',forgot = True,form=form)
@@ -23,10 +30,24 @@ def login():
 @app.route('/sign-up',methods=('GET','POST'))
 @app.route('/signup',methods=('GET','POST'))
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
     form = RegistrationForm()
     if form.validate_on_submit():
+        hashed_password=bcrypt.generate_password_hash(form.password.data)
+        user = User(username=form.username.data,email=form.email.data,password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}!','success')
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
     return render_template('sign-up.html',title='Sign Up',forgot = False,form=form)
 
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html',title='Dashboard')
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
