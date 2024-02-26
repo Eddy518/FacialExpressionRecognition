9523307888ef8ef03b3a02943a486372bedc6cd3
Eddy518 <edmwangi2@gmail.com>
2024-02-26 21:59:08 +0300

    feat: Add controls and camera to dashboard

---
+++ facial/static/video.js
@@ -0,0 +1,64 @@
+// // const video = document.querySelector("#video");
+// // const startCameraBtn = document.querySelector("#start-camera");
+// const stopCameraBtn = document.querySelector("#stop-camera");
+//
+// // stopCameraBtn.addEventListener("click", () => alert("Hey"));
+//
+// document.addEventListener("DOMContentLoaded", () => {
+//   let but = document.getElementById("start-camera");
+//   let video = document.getElementById("video");
+//   let mediaDevices = navigator.mediaDevices;
+//   but.addEventListener("click", () => {
+//     // Accessing the user camera and video.
+//     mediaDevices
+//       .getUserMedia({
+//         video: true,
+//         audio: true,
+//       })
+//       .then((stream) => {
+//         // Changing the source of video to current stream.
+//         video.srcObject = stream;
+//         video.addEventListener("loadedmetadata", () => {
+//           video.play();
+//
+//       })
+//       .catch(alert);
+//   });
+//   stopCameraBtn.onclick = function () {
+//     localStream.getVideoTracks()[0].stop();
+//     video.src = "";
+//
+//     localStream.getAudioTracks()[0].stop();
+//     audio.src = "";
+//   };
+// });
+const videoContainer = document.querySelector(".video");
+(function () {
+  const video = document.querySelector("#video");
+
+  const captureVideoButton = document.querySelector("#start-camera");
+  const stopVideoButton = document.querySelector("#stop-camera");
+
+  //Capture Video
+  captureVideoButton.onclick = function () {
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: true,
+        video: true,
+      })
+      .then((stream) => {
+        window.localStream = stream;
+        video.srcObject = stream;
+        audio.srcObject = stream;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  stopVideoButton.onclick = function () {
+    localStream.getVideoTracks()[0].stop();
+    video.src = "";
+    location.reload();
+  };
+})();
