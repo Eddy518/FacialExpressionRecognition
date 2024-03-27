e8d21f191f16efbfac305609192ed6b9380be099
Eddy518 <edmwangi2@gmail.com>
2024-03-27 18:04:59 +0300

    fix: Separate config variables in .env file

--- facial/__init__.py
+++ facial/__init__.py
@@ -1,22 +1,25 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from dotenv import load_dotenv
+
+load_dotenv('.env')
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] ='927af1257ba908bea162f42b2011047f'
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI']= os.getenv('SQLALCHEMY_DATABASE_URI')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] =  True
-app.config['MAIL_USERNAME'] = 'edmwangi2@gmail.com'
-app.config['MAIL_PASSWORD'] = 'fswo vtms qxyt kikc'
-
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
+app.config['MAIL_USE_TLS'] =  os.getenv('MAIL_USE_TLS')
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 mail = Mail(app)
 
 from facial import routes
